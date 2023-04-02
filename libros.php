7a263106506a2ca4105ce7aeba11bc06e6b5f528
Adrián Gil Reyes <126864187+Bibliotecario1993@users.noreply.github.com>
2023-04-02 18:58:24 +0100

    Funcionalidad de fecha de insersión de reserva
    En el fichero libros.php hay un input de tipo date donde el usuario selecciona DIA/MES/AÑO. Se le da a añadir y la fecha , sólo aparece 00/000/000 no está pillando la fecha insertada, para ello se adjunta el fichero libros.php y la carpeta SQl con el fichero biblioteca.sql, en el que se ha insertado un nuevo campo de tipo DATE.

--- libros.php
+++ libros.php
@@ -2,7 +2,7 @@
     include('conexion.php');
     include('admin.php');
     include('admin_consultas.php');
-?>
+?> 
 <html style="background-color: FFE633;">
 	<head>
 		
@@ -12,7 +12,7 @@
 		<meta charset="utf-8">
 		<style type="text/css">
 			tbody tr:nth-child(odd){ 
-				background: #eac633;
+				background: #eac633; 
 			}
 			
 			tbody tr:nth-child(even){
@@ -20,7 +20,7 @@
 			}
 		</style>
 	</head>
-	<body>
+	<body> 
 		<!--Libros-->
 		<div style="clear: both; padding-top: 10px">
 			<form name="form" action="admin_consultas.php" method="POST">
@@ -32,42 +32,53 @@
 				<input required type="text" name="editorial" placeholder="Editorial"><br>
 				<input required type="number" name="numpags" placeholder="Número de páginas"><br>
 				<input required type="number" name="stock" placeholder="Stock"><br>
+				<input required type="date"  name="reserva"><br>
 				<input type="submit" name="addLibro" value="Añadir" onclick="add()"><br><br>
+				<p>Confirmacion de reserva de libros : <input type="checkbox" id="reserva" name="checkBoxPermisos"></p>
 			</form>
+			<script type="text/javascript">
+				if(document.getElementById('reserva').checked){ 
+					alert('Libro reservado');
+				}
+
+			</script> 
+
+
 			<form name="form" action="admin_consultas.php" method="POST">
 				<input required type="text" name="isbnOid" placeholder="ISBN o ID">
-				<input type="submit" name="removeLibro" value="Eliminar" onclick="remove()">
+				<input type="submit" name="removeLibro" value="Eliminar" onclick="remove()"> 
 			</form>
-			<?php
-				$sql = "SELECT id, titulo, autor, genero, editorial, numero_paginas, stock, ISBN FROM libro";
+			<?php 
+				$sql = "SELECT id, titulo, autor, genero, editorial, numero_paginas, stock, reserva, ISBN FROM libro"; 
 				$resultado = $connect->query($sql) or die(mysqli_error($connect));
-			?>
+			?> 
 			<br>
 				<table border="1px solid black" cellspacing="0">
 					<thead style="background-color: #4d8cf2;">
 						<th>Id</th>
 						<th>Título</th>
-						<th>Autor</th>
+						<th>Autor</th> 
 						<th>Género</th>
-						<th>Editorial</th>
+						<th>Editorial</th> 
 						<th>Número de páginas</th>
 						<th>ISBN</th>
 						<th>Stock</th>
-					</thead>
+						<th>Fecha de reserva</th>
+					</thead> 
 					<tbody>
 			<?php
 				if (mysqli_num_rows($resultado)>0) {
 					while($valor = mysqli_fetch_assoc($resultado)) {
-						echo "<tr><td align='center'>".$valor["id"]. "</td><td align='center'>" .$valor["titulo"]. "</td><td align='center'>".$valor["autor"]."</td><td align='center'>" .$valor["genero"]. "</td><td align='center'>" .$valor["editorial"]."</td><td align='center'>" .$valor["numero_paginas"]. "</td><td align='center'>" .$valor["ISBN"]."</td><td align='center'>" .$valor["stock"]. "</td></tr>";
+						echo "<tr><td align='center'>".$valor["id"]. "</td><td align='center'>" .$valor["titulo"]. "</td><td align='center'>".$valor["autor"]."</td><td align='center'>" .$valor["genero"]. "</td><td align='center'>" .$valor["editorial"]."</td><td align='center'>" .$valor["numero_paginas"]. "</td><td align='center'>" .$valor["ISBN"]."</td><td align='center'>" .$valor["stock"]. "</td><td align='center'>" .$valor["reserva"]."</td></tr>";
 					}
 				} else {
 					echo "<tr><td colspan='8' align='center'>0 resultados</td></tr>";
 				}
-			?>
-					</tbody>
-				</table>
+			?> 
+					</tbody> 
+				</table>   
 		</div>
-			
+			  
 	</body>
 
-</html>
+</html>   
