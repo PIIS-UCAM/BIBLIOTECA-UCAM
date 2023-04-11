68b4f15bcc3a13b8d5135569047ce2c8ea697375
Adrián Gil Reyes <126864187+Bibliotecario1993@users.noreply.github.com>
2023-04-11 15:26:47 +0100

    CarnetBiblioteca.php
    Está realizada la maquetación con CSS, primero en HTML y luego la pasé al fichero .php que esta adjunto, todavía no he podido realizar la función php, el xampp me tira varios problemas, entre ellos el SQL donde no me permite ver el php dentro del localhost

---
+++ CarnetBiblioteca.php
@@ -0,0 +1,84 @@
+<html>
+
+<head>
+  <title>Carnet</title>
+  <style>
+        .padre{
+          background-color:  #004175;
+          width: 36%;
+          height: 39%;
+          position: relative;
+          border-radius: 10%;
+          border-bottom: 10%;
+        }
+
+        .carnet{
+           background-color: #F5B003;
+           color: white;
+           font-family:Arial, Helvetica, sans-serif;
+           border-radius: 10%;
+           border-bottom: 10%;
+        }
+
+
+        .photo{
+          width: 103px;
+          height: 105px;
+          position: relative;
+          right: 200px;
+        }
+
+        .lettera{
+           font-family: Arial, Helvetica, sans-serif;
+           color: whitesmoke;
+           font-size: 20px;
+        }
+
+        .letterb{
+          font-family: Arial, Helvetica, sans-serif;
+           color: whitesmoke;
+           font-size: 20px;
+        }
+
+        .letterc{
+          font-family: Arial, Helvetica, sans-serif;
+           color: whitesmoke;
+           font-size: 20px;
+        }
+
+        .letterd{
+          font-family: Arial, Helvetica, sans-serif;
+           color: whitesmoke;
+           font-size: 20px;
+        }
+
+        
+  </style> 
+
+
+</head>
+
+
+<body>
+        <center>
+        <div class="padre">
+          <center><h1 class="carnet">Biblioteca UCAM</h1></center>  
+
+            <label class="lettera">Nombre: </label><br><br>
+            <label class="letterb">Apellido:</label><br><br>
+            <label class="letterc">Fecha de nacimiento:</label><br><br>
+            <label class="letterd">DNI:</label><br><br>
+
+          <img class="photo" src="https://acortar.link/Rkd7hG">
+       
+        </div>
+      </center> 
+</body>
+
+<?php
+            /*codigo php*/  
+
+?>
+
+ </html>
+ 
