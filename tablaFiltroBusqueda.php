9320f36fdfe653c806e5fafbcc74a71e53280bfa
Adrián Gil Reyes <126864187+Bibliotecario1993@users.noreply.github.com>
2023-04-12 13:55:46 +0100

    HU005-TAREA2 Tabla maquetada
    Creación de una tabla maquetada a la espera de introducir las consultas en la misma de tal manera que sea visual para el usuario, en el fichero se encuentra una tabla maquetada con CSS colores corporativos a falta de introducirle las consultas de la BD, voy haciendo estas pequeñas cosillas para ir adelantando trabajo ya que no puedo visualizar desde xamppo pues voy creando html y posteriormente el php , ya maquetado solo falta php

---
+++ tablaFiltroBusqueda.php
@@ -0,0 +1,62 @@
+<html>
+
+
+<head>
+    <title>Tabla filtro busqueda</title>
+    <style>
+            table, th, td{
+                border: 2px solid blue;
+            }
+
+            .color{
+                background-color: #F5B003;
+            }    
+
+           
+
+    </style>    
+
+</head>
+
+
+<body>
+
+    <table style="width:25%">
+        <tr>
+          <th class="color">ID</th>
+          <th class="color">Autor</th>
+          <th class="color">ISBN</th>
+          <th class="color">Titulo</th>
+          <th class="color">Genero</th>
+        </tr>
+        <tr>
+          <td >1</td>
+          <td >1</td>
+          <td >1</td>
+          <td >10</td>
+          <td >10</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>10</td>
+          <td>10</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>10</td>
+            <td>10</td>    
+        </tr>
+          
+        </table>
+
+        <h3>Tabla de filtro de busqueda posteriormente adaptar con SQL</h3>
+
+</body>
+
+
+
+</html>
