dc34de62de2baa02c334e43975d6ae6de99cca3c
AinhoaAlbaLahoz <126873440+AinhoaAlbaLahoz@users.noreply.github.com>
2023-04-19 22:52:28 +0200

    Modificar estilos carnet

--- CarnetBiblioteca.php
+++ CarnetBiblioteca.php
@@ -2,57 +2,57 @@
 
 <head>
   <title>Carnet</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
   <style>
-        .padre{
-          background-color:  #004175;
-          width: 36%;
-          height: 39%;
-          position: relative;
-          border-radius: 10%;
-          border-bottom: 10%;
+        .card{
+          width: 30rem;
+          height: 15rem;
+          margin: 30px 0px 0px 500px;
         }
 
-        .carnet{
-           background-color: #F5B003;
-           color: white;
-           font-family:Arial, Helvetica, sans-serif;
-           border-radius: 10%;
-           border-bottom: 10%;
+        .card-img-top{
+          width: 10rem;
+          height: 11rem;
+          float: left;
         }
 
-
-        .photo{
-          width: 103px;
-          height: 105px;
-          position: relative;
-          right: 200px;
+        .card-header{
+          text-align: center;
+          background-color: #002664;
+          color: white;
+          font-weight: 900;
         }
 
-        .lettera{
-           font-family: Arial, Helvetica, sans-serif;
-           color: whitesmoke;
-           font-size: 20px;
+        .card-body{
+          background-color: #E9E9E9;
+          color: black;
+          font-weight:700;
         }
 
-        .letterb{
-          font-family: Arial, Helvetica, sans-serif;
-           color: whitesmoke;
-           font-size: 20px;
+        .card-text1{
+          margin: 0px 0px 5px 175px;
         }
 
-        .letterc{
-          font-family: Arial, Helvetica, sans-serif;
-           color: whitesmoke;
-           font-size: 20px;
+        .card-text2{
+          margin: 0px 0px 5px 175px;
         }
 
-        .letterd{
-          font-family: Arial, Helvetica, sans-serif;
-           color: whitesmoke;
-           font-size: 20px;
+        .card-text3{
+          margin: 0px 0px 5px 175px;
         }
 
-        
+        .card-text4{
+          margin: 0px 0px 5px 175px;
+        }
+        .card-text5{
+          margin: 0px 0px 5px 175px;
+        }
+
+        .card-text6{
+          margin: 0px 0px 5px 175px;
+        }
+
   </style> 
 
  
@@ -60,19 +60,20 @@
 
  
 <body>
-        <center>
-        <div class="padre">
-          <center><h1 class="carnet">Biblioteca UCAM</h1></center>  
-                                                            /*forma de recuperar los datos para ello incluimos el fichero php de alta del usuario*/       
-            <label class="lettera">Nombre: </label><br><br> <?php $nombre =  $_GET['nombre'] ?>
-            <label class="letterb">Apellido:</label><br><br> <?php $apellido =  $_GET['apellido'] ?>
-            <label class="letterc">Fecha de nacimiento:</label><br><br> <?php $fechanacimineto =  $_GET['fechanacimiento'] ?>
-            <label class="letterd">DNI:</label><br><br> <?php $dni =  $_GET['dni'] ?>
+           <!-- forma de recuperar los datos para ello incluimos el fichero php de alta del usuario    -->
 
-          <img class="photo" src="https://acortar.link/Rkd7hG">
-       
-        </div> 
-      </center> 
+<div class="card">
+<div class="card-header">BIBLIOTECA UCAM</div>
+<div class="card-body">
+        <img class="card-img-top" src="https://acortar.link/Rkd7hG">
+    <p class="card-text1">Nombre:</p><?php $nombre =  $_GET['nombre'] ?>
+    <p class="card-text2">Apellido:</p><?php $apellido =  $_GET['apellido'] ?>
+    <p class="card-text3">Fecha de nacimiento:</p> <?php $fechanacimineto =  $_GET['fechanacimiento'] ?>
+    <p class="card-text4">DNI:</p> <?php $dni =  $_GET['dni'] ?>
+    <p class="card-text5">Fecha de expedición:</p>
+    <p class="card-text6">Fecha de validez:</p>
+  </div>
+</div>
 </body>
 
 <?php
